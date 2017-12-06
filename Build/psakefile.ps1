71dbb09cc6c091bcc67962f769693b54251137cc
Ben Davies <ukbendavies@users.noreply.github.com>
2017-12-06 10:30:03 +0000

    ensure paths are handled using readable powershell constructs to convey more meaning. ensure DefaultProperties are cloned so they are not overwritten.

--- Build/psakefile.ps1
+++ Build/psakefile.ps1
@@ -5,10 +5,10 @@
 #   Invoke-psake .\build\psakefile.ps1 -properties @{"Verbose"=$true; "Debug"=$true}
 
 Properties {
-    $Base    = Resolve-Path "$PSScriptRoot\.."
+    $Base    = Split-Path (Resolve-Path $PSScriptRoot) -Parent
     $Log     = Join-Path $Base Log
     $Help    = Join-Path $Base Help
-    $Modules = Resolve-Path $Base\Modules
+    $Modules = Resolve-Path (Join-Path $Base Modules)
     $ModuleName = 'PoshHive'
     $Debug   = $false
     $Verbose = $false
@@ -16,7 +16,7 @@ Properties {
 
 FormatTaskName {
    param($taskName)
-   # make tasks magenta to distinguish from verbose output colour
+   # uaee magenta to distinguish tasks from verbose output
    Write-Host "Executing Task: $taskName" -foregroundcolor Magenta
 }
 
@@ -24,7 +24,8 @@ Task default -Depends BuildNugetPackage,
                       DebugEnvironment
 
 Task BuildNugetPackage -Depends UnitTest, GenerateHelp {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
+    $properties.Add('ModuleName', $ModuleName)
     $args = @{
         'buildFile'  = 'Tasks\buildnugetpackage.ps1';
         'properties' = $properties;
@@ -34,7 +35,7 @@ Task BuildNugetPackage -Depends UnitTest, GenerateHelp {
 }
                       
 Task UnitTest -Depends StaticAnalysis {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
     $args = @{
         'buildFile'  = 'Tasks\template.ps1';
         'properties' = $properties;
@@ -44,7 +45,7 @@ Task UnitTest -Depends StaticAnalysis {
 }
 
 Task GenerateHelp -Depends StaticAnalysis, InitialiseLog {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
     $properties.Add('Help', $Help)
     $args = @{
         'buildFile'  = 'Tasks\generatehelp.ps1';
@@ -55,7 +56,7 @@ Task GenerateHelp -Depends StaticAnalysis, InitialiseLog {
 }
 
 Task StaticAnalysis -Depends BuildModule {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
     $properties.Add('ModuleName', $ModuleName)
     $args = @{
         'buildFile'  = 'Tasks\staticanalysis.ps1';
@@ -66,7 +67,7 @@ Task StaticAnalysis -Depends BuildModule {
 }
 
 Task BuildModule -Depends InitialiseRequiredModules {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
     $args = @{
         'buildFile'  = 'Tasks\buildmodule.ps1';
         'properties' = $properties;
@@ -76,7 +77,7 @@ Task BuildModule -Depends InitialiseRequiredModules {
 }
 
 Task InitialiseRequiredModules -Depends InitialiseProperties {
-    $properties = $script:DefaultProperties
+    $properties = ($script:DefaultProperties).Clone()
     $args = @{
         'buildFile'  = 'Tasks\initialiserequiredmodules.ps1';
         'properties' = $properties;
@@ -99,24 +100,24 @@ Task InitialiseLog -Depends InitialiseProperties {
     } else {
         New-Item -Type directory -Path $Log 2>$null | Out-Null
     }
-    Write-Output "Any file Based logs will be located at: $Log"
+    Write-Output "Any file based logs will be located at: $Log"
 }
 
 task InitialiseProperties {
-    Assert ($Base -ne $null) "Base should not be null"
-    Assert ($Log -ne $null) "Log should not be null"
+    Assert ($Base -ne $null) 	"Base should not be null"
+    Assert ($Log -ne $null) 	"Log should not be null"
     Assert ($Modules -ne $null) "Modules should not be null"
-    Assert ($Debug -ne $null) "Debug should not be null"
+    Assert ($Debug -ne $null) 	"Debug should not be null"
     Assert ($Verbose -ne $null) "Verbose should not be null"
     
-    # initialise default working set of properties
-    # these are shared between all Task contexts 
-    # within this script.
-    $script:DefaultProperties = @{
-        "Base"    = $Base;
-        "Log"     = $Log;
-        "Modules" = $Modules;
-        "Debug"   = $Debug;
-        "Verbose" = $Verbose
-    }
+    # Initialise default working set of properties. These
+    # are shared between all Task contexts in this script.
+    # DefaultProperties must be cloned before modification
+	$Script:DefaultProperties = @{
+		"Base"    = $Base;
+		"Log"     = $Log;
+		"Modules" = $Modules;
+		"Debug"   = $Debug;
+		"Verbose" = $Verbose
+	 }
 }
